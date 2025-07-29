72d0b6148e1f5f5902252e2074b53be888be386e
Louis Duret-Robert <louisduret@gmail.com>
2025-07-29 21:01:50 +0200

    Slovak: add audio for some words + better table scrolling without line wrapping

---
+++ slovak/audio.js
@@ -0,0 +1,11 @@
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('audio')) {
+    const word = event.target.textContent.toLowerCase().replace('/','-');
+    console.log(word)
+    const soundFilePath = 'audio/' + word + '.m4a';
+    const audio = new Audio(soundFilePath);
+    audio.play().catch(error => {
+      console.error('Error playing sound for ' + word + ':', error);
+    });
+  }
+});
